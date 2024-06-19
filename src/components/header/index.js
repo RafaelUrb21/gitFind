4ff6ed9fac544f6aec67ee2633f318e4921b8812
Rafael Urbano <141960631+RafaelUrb21@users.noreply.github.com>
2024-06-19 19:38:01 -0300

    gitFind versão desktop

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+        <h1>GitFind</h1>
+    </div>
+  );
+}
+
+export { Header };
---
+++ src/pages/home/index.js
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import { Header } from '../../components/header';
+import background from '../../assets/background.png';
+import ItemList from '../../components/ItemList';
+import "./styles.css"
+
+function App() {
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    try {
+      const userData = await fetch(`https://api.github.com/users/${user}`);
+      const newUser = await userData.json();
+
+      if (newUser) {
+        const { avatar_url, name, bio, login } = newUser;
+        setCurrentUser({ avatar_url, name, bio, login });
+
+        const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+        const newRepos = await reposData.json();
+
+        if (newRepos.length) {
+          setRepos(newRepos);
+        }
+      }
+    } catch (error) {
+      console.error("Failed to fetch data", error);
+    }
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <div className='conteudo'>
+        <img src={background} className='background' alt='background-app' />
+        <div className='info'>
+          <div>
+            <input
+              name='usuario'
+              value={user}
+              onChange={event => setUser(event.target.value)}
+              placeholder='@username'
+            />
+            <button onClick={handleGetData}>Buscar</button>
+          </div>
+          {currentUser ? (
+            <>
+              <div className='perfil'>
+                <img src={currentUser.avatar_url} className='profile' alt='imagem de perfil' />
+                <div>
+                  <h3>{currentUser.name}</h3>
+                  <span>@{currentUser.login}</span>
+                  <p>{currentUser.bio}</p>
+                </div>
+              </div>
+              <hr />
+            </>
+          ) : null}
+          {repos?.length ? (
+            <div>
+              <h4 className='repositorio'>Repositórios</h4>
+              {repos.map(repo => (
+                <ItemList key={repo.id} title={repo.name} description={repo.description} />
+              ))}
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
